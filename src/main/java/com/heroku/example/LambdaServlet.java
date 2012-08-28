939cff604600316f5d8471ef95789e1a045ffe5f
Naaman Newbold <naaman@heroku.com>
2012-08-28 00:16:28 -0700

    Always final :rage:

--- src/main/java/com/heroku/example/LambdaServlet.java
+++ src/main/java/com/heroku/example/LambdaServlet.java
@@ -11,7 +11,7 @@ import java.util.Arrays;
 public class LambdaServlet extends HttpServlet {
   @Override
   protected void doGet(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException, IOException {
-    PrintWriter w = resp.getWriter();
+    final PrintWriter w = resp.getWriter();
     System.getProperties()
       .filter((l, r) -> r.toString().contains("1.8"))
       .sorted((l, r) -> l.toString().compareTo(r.toString()))
